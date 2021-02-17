726b624a105e3afbb950eabe450601d4e33a224b
tsotetsi <me@tsotetsithapelo.co.za>
2021-02-17 10:44:02 +0200

    Add ability to add new comment.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let send = function() {
+
+    // let request = new XMLHttpRequest();
+    // let formElement = document.querySelector("form");
+
+    // // POST to httpbin which returns the POST data as JSON
+    // request.open('POST', 'https://httpbin.org/post', async = false);
+
+    // let formData = new FormData(formElement);
+
+    // request.send(formData);
+    // console.log(request.response);
+    // console.log(JSON.parse(request.response))
+
+}
+
+
