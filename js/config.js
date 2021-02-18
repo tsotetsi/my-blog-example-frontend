bd522dfc1404d9a69197b8c6218053c1985ddcd6
tsotetsi <me@tsotetsithapelo.co.za>
2021-02-18 15:53:40 +0200

    Update api prod link.

--- js/config.js
+++ js/config.js
@@ -2,13 +2,13 @@
 // The links are for the backend(API).
 const API_URLS = {
     'dev': 'http://127.0.0.1:5000/',
-    'production': 'production-link-here'
+    'production': 'https://radiant-plateau-80785.herokuapp.com/'
 }
 
 // The current environment you are currently in.
 // Before pushing to production(netlify), update
 // the CURRENT_ENV below to production.
-const CURRENT_API_URL = 'dev';
+const CURRENT_API_URL = 'production';
 
 // Final Link for your backend API url.
 const API_LINK = API_URLS[CURRENT_API_URL];
